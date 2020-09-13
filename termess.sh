932ceedb2fbb8cacf7b311ed4a1040b96239aa7a
Patrik Drbal <drbalpat@fit.cvut.cz>
2020-09-14 00:58:10 +0200

    improve termess.sh
    tmp files now in folder generated by mktemp
    path to repair script can be relative
    participants in var instead of file
    removed useless final redirecting to file and cat-ing
    help wording tweaks
    other minor code improvements

--- termess.sh
+++ termess.sh
@@ -1,104 +1,99 @@
 #! /bin/bash
 
-# Pasrse Facebook messages in json and store them into terminal-friendly files
-# Allows easy reading and searching from terminal and using terminal tools
+# Transforms facebook message JSONs into terminal-friendly files
+# Output files can be easily read and searched through with standard terminal tools
+
 
 # CONTINUE PROMPT
-prompt () {
-    echo
-    echo "$1"
-    read -p "Press [Y] to continue: " PROCEED
-    if [ "$PROCEED" != "Y" ] ; then
+prompt() {
+    echo -e "\n$1"
+    read -p "Press [Y] to continue: " proceed
+    if [ "$proceed" != "Y" ] ; then
         echo Aborting...
         exit 1
     fi
 }
 
-# FIND PATH TO DIRECTORY WITH MESSAGES IN JSON
-PATH_FOUND=0
-find_msgs_path () {
-    if [ $PATH_FOUND -eq 0 ] ; then
-        echo
-        read -p $'Please enter a path to directory containing inbox/ and message_requests/:\n> ' -i "$HOME/Downloads/facebook/messages/" -e MSG_DIR
-        PATH_FOUND=1
-    fi
-}
-
 # UNZIP GIVEN ZIP AND CREATE DIRECTORY STRUCTURE
 unzip_and_prepare () {
-    prompt "Do you want to unzip messages in this directory?"
+    prompt "Do you want to unzip facebook data in this directory?"
     
     unzip "$1"
 
-    mkdir -p facebook/media
-    mkdir -p facebook/_CUSTOM/processed\ chats
-    mkdir -p facebook/_CUSTOM/interesting\ data
+    mkdir -p facebook/media  facebook/processed_chats
 
+    mv messages/stickers_used facebook
     mv messages facebook
-    mv facebook/messages/stickers_used facebook/
 
-    cd facebook/messages/inbox
-    mv $( for folder in * ; do ls $folder | grep -qE 'photos|videos|gifs|audio|files' && echo $folder ; done ) ../../media/
-    cd -
+    (
+        cd facebook/messages/inbox
+        mv $( for folder in * ; do ls $folder | grep -qE 'photos|videos|gifs|audio|files' && echo $folder ; done ) ../../media/
+    )
+}
+
+# FIND PATH TO DIRECTORY WITH JSONS
+path_found=0
+find_msgs_path() {
+    if (( ! $path_found )) ; then
+        read -p $'\nPlease enter a path to directory containing inbox/ and message_requests/:\n> ' -i "$HOME/Downloads/facebook/messages/" -e msg_dir
+        path_found=1
+    fi
 }
 
 # REPAIR JSON BAD ENCODING
-repair_json () {
-    range=$(echo {1..32}) # max num of message files
-    CURRENT_DIR="$(pwd)"
+repair_json() (
+    script=$( realpath "$1" )
 
-    # inbox
-    cd "$MSG_DIR"/inbox
-
-    for file in $range ; do
-        "$1" $file
+    cd "$msg_dir"/inbox
+    for file in $(echo {1..35}) ; do
+        "$script" $file
     done
 
-    # message_reguests
-    cd "$MSG_DIR"/message_requests
-
-    for file in $range ; do
-        "$1" $file
+    cd "$msg_dir"/message_requests
+    for file in $(echo {1..5}) ; do
+        "$script" $file
     done
+)
 
-    cd "$CURRENT_DIR"
-}
-
-# PROCCESS CHAT
-format_chat () {
-    # put participant names in bash array - takes first file to find names
-    jq -r '.participants[].name' "$1"/message_1.json | sort > fbjson_participants1
+# PROCESS CHAT
+format_chat() {
+    lc_time_tmp=$LC_TIME
+    work_folder=$( mktemp -d )
+    special_name='Patrik Drbal'
     index=0
+
+    # put participant names in bash array - takes first file to find names
+    participants1=$( jq -r '.participants[].name' "$1"/message_1.json | sort )
     while read name ; do
         names[$index]="$name"
         index=$(( $index + 1 ))
-    done <<<"$( cat fbjson_participants1 )"
+    done <<<$( echo "$participants1" )
 
     # put participant names in bash array - former participants
-    cat "$1"/* | grep -Ei '(removed .* from the group)|(left the group)' | sed -E -e 's/.*removed (.*) from the group.*/\1/' -e 's/.*"(.*) left the group.*/\1/' | sort | uniq > fbjson_participants2
+    participants2=$( cat "$1"/* | grep -Ei '(removed .* from the group)|(left the group)' | sed -E -e 's/.*removed (.*) from the group.*/\1/' -e 's/.*"(.*) left the group.*/\1/' | sort | uniq )
     while read name ; do
         if [[ $name = '' ]] ; then
             continue
         fi
         names[$index]="$name"
         index=$(( $index + 1 ))
-    done <<<"$( diff -u fbjson_participants1 fbjson_participants2 | grep -E '^\+' | sed -e 's/+//' -e 's/++.*//' | grep -v '^$' )"
+    done <<<$( diff -u <( echo -e "$participants1" ) <( echo -e "$participants2" ) | grep -E '^\+' | sed -e 's/+//' -e 's/++.*//' | grep -v '^$' )
 
     # find longest name - used later in output formatting
-    MAX_NAME_LEN=0
+    max_name_len=0
     for name in "${names[@]}" ; do 
-        NAME_LEN=${#name}
-        if [ $NAME_LEN -gt $MAX_NAME_LEN ] ; then
-            MAX_NAME_LEN=$NAME_LEN
+        name_len=${#name}
+        if [ $name_len -gt $max_name_len ] ; then
+            max_name_len=$name_len
         fi
     done
 
     # concatenate everything into one file 
-    cat $( ls "$1"/* | sort -V ) > fbjson # cat alone does not give correct order 
+    cat $( ls "$1"/* | sort -V ) > "$work_folder/chat_tmp" # cat alone does not give correct order 
 
     LC_TIME=en_US.utf8 # for english date output
 
-    sed -E 's:\\n: *NEW LINE* :g' fbjson | \
+    sed -E 's:\\n: *NEW LINE* :g' "$work_folder/chat_tmp" | \
     # display data in one row
     jq -r '.messages[] | "\(.timestamp_ms) \(.sender_name) \(.content)"' | \
     # reverse message order
@@ -111,74 +106,68 @@ format_chat () {
     # date and time color
     sed -E \
     "s/[[:alpha:]]{3} [[:digit:]]{2} [[:alpha:]]{3} [[:digit:]]{4}, [[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}/$( echo -e '\033[32m&\033[0m' )/" \
-    > fbjson2
+    > "$work_folder/chat"
+
+    # names color
+    for name in "${names[@]}" ; do 
+        if [ "$name" = "$special_name" ] ; then
+            sed -Ei "s/$name/$( echo -e '\033[31m&\033[0m' )/" "$work_folder/chat"
+        else
+            sed -Ei "s/$name/$( echo -e '\033[36m&\033[0m' )/" "$work_folder/chat"
+        fi
+    done
 
     # set *** string as separator
     for name in "${names[@]}" ; do 
-        sed -Ei "s/$name/***&***/" fbjson2
+        sed -Ei "s/$name/***&***/" "$work_folder/chat"
     done
 
     LC_TIME=cs_CZ.UTF-8 # for correct lenght of czech symbols
 
-    # text alignment
+    # text alignment and final output
     awk -F'***' "{
         name_length = length(\$2)
-        for (i = 0; i < $MAX_NAME_LEN - name_length; i++)
+        for (i = 0; i < $max_name_len - name_length; i++)
             \$2 = \$2\" \"
 
         printf ( \"%s %s %s\n\", \$1, \$2, \$3 )
-    }" fbjson2 > fbjson
+    }" "$work_folder/chat"
 
-    # names color
-    for name in "${names[@]}" ; do 
-        if [ "$name" = "Patrik Drbal" ] ; then
-            sed -Ei "s/$name/$( echo -e '\033[31m&\033[0m' )/" fbjson
-        else
-            sed -Ei "s/$name/$( echo -e '\033[36m&\033[0m' )/" fbjson
-        fi
-    done
-
-    cat fbjson
-
-    rm fbjson*
+    rm -rf "$work_folder"
+    LC_TIME=$lc_time_tmp
 }
 
 # PROCCESS ALL CHATS AND SAVE THEM TO FILES
-proccess_all_chats () {
+proccess_all_chats() {
     prompt "Do you want to generate message files in this directory?"
 
-    find_msgs_path
+    number_of_chats=$(( $( ls -1q "$msg_dir"/inbox | wc -l ) + $( ls -1q "$msg_dir"/message_requests | wc -l ) ))
+    counter=1
 
-    NUMBER_OF_CHATS=$(( $( ls -1q "$MSG_DIR"/inbox | wc -l ) + $( ls -1q "$MSG_DIR"/message_requests | wc -l ) ))
-    COUNTER=1
+    for folder in "$msg_dir"/{inbox,message_requests}/* ; do
+        echo "[$counter / $number_of_chats]:    $folder"
+        counter=$(( $counter + 1 ))
 
-    for folder in "$MSG_DIR"/{inbox,message_requests}/* ; do
-        echo "[$COUNTER / $NUMBER_OF_CHATS]:    $folder"
-        COUNTER=$(( $COUNTER + 1 ))
-
-        format_chat "$folder" > $( echo $folder | sed -E 's/.*\/([^_]+).*$/\1/' ) # old: awk -F'/' '{ print $NF }'
+        format_chat "$folder" > $( echo $folder | sed -E 's_.*/([^_]+).*$_\1_' )
     done
 
     echo -e '\033[33m\nDONE\033[0m'
 }
 
-
-
-# PROCCESS FLAG OPTIONS
+# PROCESS FLAG OPTIONS
 while getopts ":hu:r:af:" opt ; do
   case ${opt} in
     h )
-        echo "-u <filename>                     Unzips given zip with facebook messages.
-                                  Prepares directory structure."
+        echo "-u <filename>                     Unzip archive with facebook messages.
+                                  Prepare directory structure."
         echo
-        echo "-r <filename>                     Uses custom or packed script to repair json bad encoding.
-                                  Script is called inside given inbox folder and modifies it.
-                                  You must enter absolute path to the script "
+        echo "-r <filename>                     Use custom or packed script to repair json bad encoding.
+                                  Path can be absolute or relative."
         echo
-        echo "-a                                Proccess all chats and saves them in current directory."
+        echo "-a                                Process all chats and save them in current directory."
         echo
-        echo "-f <chat_folder>                  Proccesses and displays given chat.
-                                  Does not save it."
+        echo "-f <chat_folder>                  Process and display chat.
+                                  Output is not saved."
     ;;
 
     u )
@@ -187,13 +176,11 @@ while getopts ":hu:r:af:" opt ; do
 
     r )
         find_msgs_path
-
         repair_json "$OPTARG"        
     ;;
 
     a )
         find_msgs_path
-
         proccess_all_chats
     ;;
 
@@ -208,7 +195,7 @@ while getopts ":hu:r:af:" opt ; do
     ;;
 
     : )
-        echo "Invalid option: $OPTARG requires a filename" >&2
+        echo "Invalid option '$OPTARG' requires a filename" >&2
         exit 2
     ;;
   esac
